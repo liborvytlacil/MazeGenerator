5919c4118aa463a483a47c089fb139e04c404f4c
Libor Vytlačil <libor.vytlacil@gmail.com>
2020-09-23 23:08:16 +0200

    Initial commit

---
+++ MazeGenerator.cpp
@@ -0,0 +1,12 @@
+﻿// MazeGenerator.cpp : Defines the entry point for the application.
+//
+
+#include "MazeGenerator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
